80315a1f102153725ce9f683d030625fb6df88dc
llllkatjallll <katja.rempel@yahoo.de>
2025-12-19 09:01:43 +0100

    added timer and sound

--- bring-your-own-beamer/sketch.js
+++ bring-your-own-beamer/sketch.js
@@ -9,9 +9,10 @@ let timeAcitve = 6000;
 
 //bilder
 let myBackground;
-let laterneAn, laterneAus;
+let laterneAn, laterneAus,laterne2An, laterne2Aus;
 let handZu, handAuf;
-let flamme1;
+let flamme1,shine;
+let scoreImage;
 let flames = [];
 
 //hand movement
@@ -21,9 +22,13 @@ let darkness = 210;
 // feuerwerk
 let fireworks = [];
 
+//trace
+let snowflakes = [];
+
 //laternen
-let latMaxSize = 350;
+let latMaxSize = 230;
 let latMinSize = 80;
+let activationSound,secondActivationSound;
 
 function preload() {
   // HandPose Modell laden
@@ -31,13 +36,21 @@ function preload() {
   myBackground = loadImage("background.png");
   laterneAn = loadImage("laterneAn.png");
   laterneAus = loadImage("laterneAus.png");
+  laterne2An = loadImage("laterne2An.png");
+  laterne2Aus = loadImage("laterne2Aus.png");
   handAuf = loadImage("handAuf_tr.png");
   handZu = loadImage("handZu.png");
   flamme1 = loadImage("images/flamme1.png");
+  myFont = loadFont('fonts/Oldenburg-Regular.ttf');
+  scoreImage = loadImage("pointsScroll.png");
+  shine = loadImage("images/shine.png");
+    activationSound = loadSound("sounds/holy-spell-cast-450460.mp3");
+    secondActivationSound = loadSound("sounds/fx-light-90387.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont(myFont);
 
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
@@ -86,14 +99,38 @@ function draw() {
       }
     } */
 
+    // Iterate through the particle array
+  // We loop backwards because we are removing items from the array
+  for (let i = snowflakes.length - 1; i >= 0; i--) {
+    let p = snowflakes[i];
+    p.update();
+    p.show();
+
+    // Remove particle if it becomes invisible (alpha <= 0)
+    if (p.finished()) {
+      snowflakes.splice(i, 1);
+    }
+  }  
 
   flames[0].display();
 
+
+push();
+ scoreImage.resize(0, 170);
+ tint(255, 200);
+
+  image(scoreImage, 10, 10, );
+pop();
+
   // Punktestand anzeigen
-  fill(255);
+  // fill dark brown
+  fill(101, 67, 33);
   textSize(24);
   textAlign(LEFT, TOP);
-  text("Licht an: " + punktestand, 20, 20);
+ textSize(70);
+  text( punktestand + "/" + laternen.length, 110, 30);
+    textSize(28);
+  text("Laternen ", 120, 115);
 }
 
 function gotHands(results) {
@@ -152,12 +189,21 @@ function calculateHandPositions(hand, flame) {
   flame.position.x = center.x;
   flame.position.y = center.y;
 
+/*   if (flame.position.x !== flame.prevPosition.x || flame.position.y !== flame.prevPosition.y) {
+    for (let i = 0; i < 10; i++) {
+      snowflakes.push(new Particle(flame.position.x + random(-10, 10), flame.position.y + random(-10, 10)));
+    }
+  }
+  flame.prevPosition = flame.position.copy(); */
+
+
 }
 
 
 class Flame {
   constructor(x, y) {
     this.position = new p5.Vector(x, y);
+    this.prevPosition = this.position.copy();
     this.size = 150;
     this.prevHandOpen = true;
     this.isHandOpen = true;
@@ -175,7 +221,8 @@ class Flame {
     push();
     imageMode(CENTER);
     fill(255, 150, 0, this.saturation/2);
-    circle(this.position.x,this.position.y,this.size + sin(millis() / 100 + this.wobbleOffset) * 5.15);
+/*     circle(this.position.x,this.position.y,this.size + sin(millis() / 100 + this.wobbleOffset) * 5.15); */
+    image(shine, this.position.x, this.position.y, this.size+ sin(millis() / 100 + this.wobbleOffset) * 5.15, this.size+ sin(millis() / 100 + this.wobbleOffset) * 5.15);
     tint(255, this.saturation);
     image(flamme1, this.position.x, this.position.y, this.size, this.size);
     
@@ -188,6 +235,7 @@ class Laterne {
   constructor(x, y, size) {
     this.position = new p5.Vector(x, y);
     this.size = size;
+    this.design = floor(random(0,2)); // 0 oder 1
 
     this.speed = map(size, latMinSize, latMaxSize, 0.4, 1.3);
     this.activated = false;
@@ -195,38 +243,52 @@ class Laterne {
     this.activatedTimer = 0;
     this.wobbleOffset = random(1000); // Damit sie nicht alle gleich wackeln
     this.color = color(random(200, 255), random(100, 200), 50); // Zufällige warme Farbe
+    
+    // Timing properties
+    this.activationTime = 0;
+    this.stayOnDuration = 20000; // 20 seconds in milliseconds
+    this.fadeDuration = 2000; // 8 seconds in milliseconds
+    this.fadeProgress = 0; // 0 = fully on, 1 = fully faded
   }
 
   activate() {
     this.activated = true;
-    // Hier könnte man noch einen Sound abspielen
+    this.activationTime = millis(); // Store when lamp was activated
+    let soundChoice = random(1);
+    if (soundChoice < 0.5) {
+      secondActivationSound.setVolume(random(0.7, 1.0));
+      secondActivationSound.play(); 
+      return;
+    }
+   
+    activationSound.setVolume(random(0.7, 1.0));
+    activationSound.play(); 
   }
 
   update() {
     // Wenn aktiviert, steigen sie schnell nach oben (Himmel)
     if (this.activated) {
-      //this.position.y -= 1; // Steiggeschwindigkeit
-      if (this.hidden == false) {
+      let elapsedTime = millis() - this.activationTime;
+      
+      // After staying on for stayOnDuration, start fading
+      if (elapsedTime > this.stayOnDuration) {
+        let fadeTime = elapsedTime - this.stayOnDuration;
+        this.fadeProgress = constrain(fadeTime / this.fadeDuration, 0, 1);
         
-        //this.activatedTimer = this.activatedTimer + 1;
-
-       /*  if (this.activatedTimer > timeAcitve) {
-          this.hidden = true;
-        } */
+        // When fade is complete, deactivate the lamp
+        if (this.fadeProgress >= 1) {
+          this.activated = false;
+          this.fadeProgress = 0;
+          punktestand--; // Decrease score when lamp goes out
+        }
       }
 
       this.position.y += sin(millis() / 500 + this.wobbleOffset) * 0.15;
 
     } else {
       // Wenn aus, schweben sie nur leicht auf und ab (Wobble)
-      //this.position.y += sin(millis() / 500 + this.wobbleOffset) * 0.25;
-
       this.position.y -= this.speed;
     }
-
-   /*  if (this.hidden) {
-      this.size = 0;
-    } */
 
     // Wenn Laterne aus dem Bild fliegt, resetten wir sie unten (Endlos-Spiel)
     if (this.position.y < -200) {
@@ -244,10 +306,30 @@ class Laterne {
   display() {
     noStroke();
     if (this.activated == true) {
+      // Calculate fade effect (1 = full brightness, 0 = completely faded)
+      let brightness = 1 - this.fadeProgress;
+      //let currentSize = this.size * brightness;
+      let glowSize = this.size * brightness;
+      
       // AN: Leuchtender Kreis + Schein
       // Schein (Glow)
-      fill(red(this.color), green(this.color), blue(this.color), 100);
-      circle(this.position.x, this.position.y, this.size);
+      fill(red(this.color), green(this.color), blue(this.color), 50 * brightness);
+      circle(this.position.x, this.position.y, glowSize);
+      
+      // Progress indicator during stayOnDuration
+      let elapsedTime = millis() - this.activationTime;
+      if (elapsedTime <= this.stayOnDuration) {
+        let progress = elapsedTime / this.stayOnDuration;
+        let angle = (1 - progress) * TWO_PI; // Start full, decrease to 0
+        
+        push();
+        stroke(255, 214, 99,120); // Yellow contour
+        strokeWeight(3);
+        noFill();
+        arc(this.position.x, this.position.y, this.size + 5, this.size + 5, -HALF_PI, -HALF_PI + angle);
+        pop();
+      }
+      
       // Kern
       fill(this.color);
       if (handOpenedOnce) {
@@ -256,7 +338,12 @@ class Laterne {
       }
       push();
       imageMode(CENTER);
-      image(laterneAn, this.position.x, this.position.y, this.size, this.size);
+      //tint(255, 255 * brightness); // Apply fade to lamp image
+      if(this.design == 0){
+        image(laterneAn, this.position.x, this.position.y, this.size, this.size);
+      }else{
+        image(laterne2An, this.position.x, this.position.y, this.size, this.size);
+      }
       pop();
       //circle(this.position.x, this.position.y, 30);
     } else {
@@ -264,8 +351,11 @@ class Laterne {
       fill(50, 40, 60, 200); // Dunkelviolett/Grau
       push();
       imageMode(CENTER);
-      //rect(this.position.x, this.position.y, 30, 40, 5);
-      image(laterneAus, this.position.x, this.position.y, this.size, this.size);
+      if(this.design == 0){
+        image(laterneAus, this.position.x, this.position.y, this.size, this.size);
+      }else{
+        image(laterne2Aus, this.position.x, this.position.y, this.size, this.size);
+      }
       pop();
     }
   }
@@ -308,6 +398,43 @@ class Firework {
 }
 
 class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    
+    // Give the particle a random initial velocity (explosion effect)
+    this.vx = random(-0.4, 0.4); 
+    this.vy = random(-0.4, 0.4); 
+    
+    this.alpha = 100; // Opacity
+    this.size = random(4, 6);
+    
+    // Set color based on frameCount to cycle through rainbow colors
+    this.hue = frameCount % 20; 
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Physics: Apply Gravity
+    this.vy += 0.05; 
+    
+    // Decrease lifespan (fade out)
+    this.alpha -= 1.5; 
+  }
+
+  finished() {
+    return this.alpha < 0;
+  }
+
+  show() {
+    fill(255, 255, 255, this.alpha);
+    ellipse(this.x, this.y, this.size);
+  }
+}
+
+/* class Particle {
   constructor(x, y, hue, isBig) {
     this.pos = createVector(x, y);
     this.hue = hue;
@@ -344,4 +471,4 @@ class Particle {
     strokeWeight(this.size);
     point(this.pos.x, this.pos.y);
   }
-}
+} */
